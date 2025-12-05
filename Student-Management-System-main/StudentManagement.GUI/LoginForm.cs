2733115088adb64c143eae28c2363d73967791de
voan2111 <voan2111@gmail.com>
2025-12-05 10:17:23 +0700

    Add files via upload

---
+++ Student-Management-System-main/StudentManagement.GUI/LoginForm.cs
@@ -0,0 +1,82 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolMangementSystem
+{
+    public partial class LoginForm : Form
+    {
+        MySqlConnection connect = new MySqlConnection("Server=localhost;Port=3307;Database=StudentManagement;Uid=root;Pwd=123456;");
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            if (username.Text == "" || password.Text == "")
+            {
+                MessageBox.Show("Please fill all blank fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    connect.Open();
+                    String selectData = "SELECT * FROM users WHERE username = @username AND password = @password";
+                    using (MySqlCommand cmd = new MySqlCommand(selectData, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username.Text.Trim());
+                        cmd.Parameters.AddWithValue("@password", password.Text.Trim());
+
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+
+                        if (table.Rows.Count >= 1)
+                        {
+                            MessageBox.Show("Login Successfully!", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            MainForm mForm = new MainForm();
+                            mForm.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Incorrect Username/Password", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error connecting Database: " + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+        private void showPass_CheckedChanged(object sender, EventArgs e)
+        {
+            password.PasswordChar = showPass.Checked ? '\0' : '*';
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+        }
+    }
+}
