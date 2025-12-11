c3a480cda280c49589bd26926868e7cdcfebc191
Quach Van Truong <kenkra999@gmail.com>
2025-12-11 22:23:35 +0700

    4nd

--- StudentManagement.GUI/LopMonForm.Designer.cs
+++ StudentManagement.GUI/LopMonForm.Designer.cs
@@ -2,8 +2,15 @@
 {
     partial class LopMonForm
     {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -24,7 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Text = "LopMonForm";
-            // ĐÃ XÓA DÒNG GÂY LỖI: this.Load += ... LopMonForm_Load_1
+
+            // QUAN TRỌNG: File Designer này phải TRỐNG (không có các dòng this.Load += ...)
+            // Lý do: Bạn đã viết code giao diện và sự kiện Load thủ công bên file .cs rồi.
         }
 
         #endregion
